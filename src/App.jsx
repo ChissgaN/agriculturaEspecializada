86f4e77a47a2e88c74e8c14eb09d34b61e6eba2b
ChissgaN <coderchissgan@gmail.com>
2024-04-05 14:52:26 -0600

    beginnign

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import "./App.css";
 import NavBar from "./Components/NavBar";
 import SliderImg from "./Components/Slider";
 import { NextUIProvider } from "@nextui-org/react";
+import Beginning from "./Components/beginning";
+
 function App() {
   return (
     <NextUIProvider>
       <div className="">
         <NavBar />
-        <SliderImg />
+        <div className="relative h-full">
+          <SliderImg />
+          <div className="absolute inset-0 flex w-[70%]">
+            <Beginning />
+          </div>
+        </div>
       </div>
     </NextUIProvider>
   );

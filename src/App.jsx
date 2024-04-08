a74a8ec5fe5ad27069c7557dae5b20626c05399e
Alan-Britoo <rotcarrillo_8@hotmail.com>
2024-04-08 18:56:19 -0500

    animacion cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,24 @@ import { NextUIProvider } from "@nextui-org/react";
 import Beginning from "./Components/beginning";
 
 function App() {
-  
   return (
     <NextUIProvider>
       <div className="md:w-full">
         <NavBar />
 
-
-   
-
-     
-       
-
         <div className="relative h-auto">
-
           <SliderImg />
           <div className="absolute inset-0 flex w-[100%]">
             <Beginning />
           </div>
         </div>
+      </div>
 
+      <div className="flex justify-center  my-10 ">
+        <div className=" grid lg:grid-cols-3 lg:gap-10 md:grid-cols-2 md:gap-6">
+          <Categorias />
+        </div>
       </div>
-     
-     <div className="flex justify-center  my-10 ">
-     <div className=" grid lg:grid-cols-3 lg:gap-10 md:grid-cols-2 md:gap-6">
-      <Categorias/>
-      </div>
-      
-     </div>
-      
     </NextUIProvider>
   );
 }
--- src/Components/beginning.jsx
+++ src/Components/beginning.jsx
@@ -9,23 +9,27 @@ export default function Beginning() {
         <h1 className="text-[35px] mb-2 text-white">
           ¡Comienza a sembrar hoy!
         </h1>
-        <div className="flex gap-5"> 
-        <div className="flex items-center w-[50%]  max-sm:w-[100%] bg-white px-3 rounded-[10px] hover:border-[2px] hover:border-gray-500 transition-[5s] hover:scale-110  duration-300 ease-in-out">
-          <img src={search} alt="Search" className="mr-2  hover:rounded-[10px] p-2 transition-[5s]  hover:scale-110  duration-300 ease-in-out" />
-          <input
-            className="w-[100%] h-[40px] bg-transparent focus:outline-none"
-            type="text"
-            placeholder="¿Qué quieres sembrar hoy?"
-          />
+        <div className="flex gap-5">
+          <div className="flex items-center w-[50%]  max-sm:w-[100%] bg-white px-3 rounded-[10px] transition-[5s] hover:scale-105  duration-200 ease-in-out">
+            <img
+              src={search}
+              alt="Search"
+              className="mr-2   p-2   hover:scale-105  duration-200 ease-in-out"
+            />
+            <input
+              className="w-[100%] h-full bg-transparent focus:outline-none flex items-center justify-center content-center pt-0 mt-0 pb-1"
+              type="text"
+              placeholder="¿Qué quieres sembrar hoy?"
+            />
+          </div>
+          <Button
+            color="success"
+            className="ml-4 max-sm:ml-2 bg-green-100 hover:bg-green-200 transition-[5s]  hover:scale-110  duration-300 ease-in-out rounded-[10px] py-[13px] px-6 text-gray-500"
+          >
+            Buscar
+          </Button>
         </div>
-        <Button
-        color="success"
-        className="ml-4 max-sm:ml-2 bg-green-100 hover:bg-green-200 transition-[5s]  hover:scale-110  duration-300 ease-in-out rounded-[10px] py-[13px] px-6 text-gray-500">
-        Buscar
-      </Button>
       </div>
-      </div>
-      
     </section>
   );
 }
